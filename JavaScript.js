0d8af6586f0b1c08d6cdde896e958a68c493d9b8
Irfan Tariq <tariqirf@corp.contoursoftware.net>
2023-01-18 14:00:22 +0500

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,35 @@
+function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var nameError = document.getElementById("name-error");
+    var emailError = document.getElementById("email-error");
+    var passwordError = document.getElementById("password-error");
+  
+    var emailRegex = /^\S+@\S+\.\S+$/;
+    var isEmailValid = emailRegex.test(email);
+  
+    if (!name) {
+      nameError.innerHTML = "Please Type Your Name";
+      document.getElementById("name").classList.add("error-border");
+    } else {
+      document.getElementById("name").classList.remove("error-border");
+    }
+  
+    if (!isEmailValid) {
+      emailError.innerHTML = "Invalid Email Address";
+      document.getElementById("email").classList.add("error-border");
+    } else {
+      emailError.innerHTML = "";
+      document.getElementById("email").classList.remove("error-border");
+    }
+  
+    if (password.length < 8) {
+      passwordError.innerHTML = "Password Must Be At Least 8 Characters";
+      document.getElementById("password").classList.add("error-border");
+    } else {
+      passwordError.innerHTML = "";
+      document.getElementById("password").classList.remove("error-border");
+    }
+  }
+  
